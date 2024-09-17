66ab396f4523f732701e08a0d2e490cfbdd5e511
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-09-17 02:24:32 -0300

    add syncpack

---
+++ .syncpackrc.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/** @type {import("syncpack").RcFile} */
+const config = {
+  $schema: "./node_modules/syncpack/dist/schema.json",
+  lintSemverRanges: false,
+  lintFormatting: false,
+  versionGroups: [
+    {
+      label: "Use workspace protocol when developing local packages",
+      dependencies: ["shared"],
+      dependencyTypes: ["peer"],
+      pinVersion: "workspace:*",
+    },
+    {
+      label: "Use '*' under 'peerDependencies' everywhere",
+      packages: ["**"],
+      dependencies: ["**"],
+      dependencyTypes: ["peer"],
+      pinVersion: "*",
+    },
+    {
+      label:
+        "Ensure all packages use whatever version the expo-app package is using",
+      dependencies: ["react-native", "react-native-web", "react-native-svg"],
+      packages: ["**"],
+      snapTo: ["expo-app"],
+    },
+  ],
+}
+
+export default config
