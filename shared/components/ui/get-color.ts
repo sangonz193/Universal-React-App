d0509e2a22c8376e8b067bc80568fb58d6c6c469
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-09-17 02:13:09 -0300

    add get color function

---
+++ shared/components/ui/get-color.ts
@@ -0,0 +1,12 @@
+import { variables } from "./gluestack-ui-provider/config"
+
+export const DEFAULT_SCHEMA = "light"
+
+export function getColor(
+  color: keyof (typeof variables)["dark"] extends `--color-${infer TColor}`
+    ? TColor
+    : unknown,
+  schema: keyof typeof variables | null | undefined,
+) {
+  return `rgb(${variables[schema || DEFAULT_SCHEMA][`--color-${color}`]})`
+}
